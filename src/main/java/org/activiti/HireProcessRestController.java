3a54ed50e6b15a6a1ef8f859e32b50adaee9acd3
lornezhang <zhangxiaolong@richinfo.cn>
2017-08-15 15:30:30 +0800

    添加activiti mysql数据库支持

--- src/main/java/org/activiti/HireProcessRestController.java
+++ src/main/java/org/activiti/HireProcessRestController.java
@@ -1,13 +1,14 @@
 package org.activiti;
 
-import org.activiti.engine.RuntimeService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.Collections;
 import java.util.Map;
+
+import org.activiti.engine.RuntimeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HireProcessRestController {
